20f0854761bcb73d12477be97a1eed3e76eea36d
lepisma <abhinav.tushar.vs@gmail.com>
2014-04-22 18:18:36 +0530

    Avoid camera unreleased error

--- camera.py
+++ camera.py
@@ -2,10 +2,6 @@
 
 import cv2
 
-# Camera streams
-CAPTURE_1 = cv2.VideoCapture(0)
-CAPTURE_2 = cv2.VideoCapture(1)
-
 #-----------------------------
 # Merging two channels
 def make_anaglyph(first, second):
@@ -17,6 +13,11 @@ def make_anaglyph(first, second):
 #------------------------------
 # Returns true if two cameras are found
 def two_cameras():
+
+	# Camera streams
+	CAPTURE_1 = cv2.VideoCapture(0)
+	CAPTURE_2 = cv2.VideoCapture(1)
+
 	capture_1_flag = CAPTURE_1.retrieve()[0]
 	capture_2_flag = CAPTURE_2.retrieve()[0]
 
@@ -26,7 +27,14 @@ def two_cameras():
 			print "- Check if you attached the second camera"
 		if capture_1_flag == False:
 			print "- Your primary camera isn't working properly"
+
+		CAPTURE_1.release()
+		CAPTURE_2.release()
+
 		return False
 	else:
 		print "Two cameras found"
+		CAPTURE_1.release()
+		CAPTURE_2.release()
+		
 		return True
--- hablar.py
+++ hablar.py
@@ -12,14 +12,18 @@ import cv2
 import camera
 import connections
 
+CONNECTION_PORT = 1111
+
 #---------------------------
 # Becoming server
-def hablar_server():
+def hablar_server(port):
 	host = 'localhost'
-	port = 11111
+	port = port
 
 	sock = connections.create_server_here(host, port)
 	sock.listen(10)
+	print "Server running"
+	print "Waiting for connections . . ."
 	conn, adr = sock.accept()
 	stream_data = conn.recv(1024)
 
@@ -43,17 +47,21 @@ def hablar_server():
 
 #---------------------------
 # Becoming client
-def hablar_client(host):
+def hablar_client(host, port):
 	host = host
-	port = 11111
+	port = port
 
 	sock = connections.connect_to(host, port)
+	print "Connected to server"
 
 	if camera.two_cameras() == True:
 		print "Hablar running in 3D mode . . ."
 
-		ret_1, frame_1 = camera.CAPTURE_1.read()
-		ret_2, frame_2 = camera.CAPTURE_2.read()
+		CAPTURE_1 = cv2.VideoCapture(0)
+		CAPTURE_2 = cv2.VideoCapture(1)
+
+		ret_1, frame_1 = CAPTURE_1.read()
+		ret_2, frame_2 = CAPTURE_2.read()
 
 		while ret_1 and ret_2:
 			frame = camera.make_anaglyph(frame1, frame2)
@@ -66,12 +74,14 @@ def hablar_client(host):
 			sock.send(string_data)
 			sock.send('done')
 
-			ret_1, frame_1 = camera.CAPTURE_1.read()
-			ret_2, frame_2 = camera.CAPTURE_2.read()
+			ret_1, frame_1 = CAPTURE_1.read()
+			ret_2, frame_2 = CAPTURE_2.read()
 	else:
 		print "Hablar running in 2D mode"
 
-		ret, frame = camera.CAPTURE_1.read()
+		CAPTURE_1 = cv2.VideoCapture(0)
+
+		ret, frame = CAPTURE_1.read()
 		while ret:
 			encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
 			result, imgencode = cv2.imencode('.jpg', frame, encode_param)
@@ -81,6 +91,24 @@ def hablar_client(host):
 			sock.send(string_data)
 			sock.send('done')
 
-			ret, frame = camera.CAPTURE_1.read()
+			ret, frame = CAPTURE_1.read()
 
 #----------------------------
+# Command line argument handling
+
+parser = argparse.ArgumentParser(description = 'Hablar is a 3D video chat application.')
+parser.add_argument('-m', '--mode',
+				help = 'Set the mode of hablar: either server (you are setting up the call) or client (you are invited to a call)',
+				required = True)
+parser.add_argument('-s', '--server',
+				help = 'Set the IP ')
+args = vars(parser.parse_args())
+
+if args['mode'] == 'server':
+	hablar_server(CONNECTION_PORT)
+
+elif args['mode'] == 'client':
+	if args['server']:
+		hablar_client(str(args['server']), CONNECTION_PORT)
+	else:
+		hablar_client('localhost', CONNECTION_PORT)
