9a1ad71993b4babda5834b2d0d81fd733972f75e
lepisma <abhinav.tushar.vs@gmail.com>
2014-05-18 03:26:52 +0530

    Remove stream.sh

--- stream.sh
+++
@@ -1,2 +0,0 @@
-cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=x264{keyint=60,idrint=2},vcodec=h264,vb=400,width=368,heigh=208,acodec=mp4a,ab=32 ,channels=2,samplerate=22100}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=asf,dst=:8082/stream.wmv}}' --no-sout-audio
-
